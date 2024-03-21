8bf9975810727e7b33e4f6b00e290eca0e6887b5
Juan Leandro Leites Barrios <laloleites@gmail.com>
2024-03-21 13:13:09 +0100

    Improved keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,8 +1,13 @@
--- Save MacOS style
+-- Saving shortcuts
+local n_save = ':w<CR>'
+local i_save = '<Esc>:w<CR>i'
 
-vim.keymap.set('n', '<D-s>', ':w<CR>', { desc = 'Save ala MacOS' })
-vim.keymap.set('i', '<D-s>', '<Esc>:w<CR>i', { desc = 'Save ala MacOS' })
-vim.keymap.set('n', '<Leader>w>', ':w<CR>', { desc = 'Save with Leader' })
+vim.keymap.set('n', '<D-s>', n_save, { desc = 'Save ala MacOS' })
+vim.keymap.set('i', '<D-s>', i_save, { desc = 'Save ala MacOS' })
 
+vim.keymap.set('n', '<C-s>', n_save, { desc = 'Save with Control' })
+vim.keymap.set('i', '<C-s>', i_save, { desc = 'Save with Control' })
+
+-- Navigate buffers
 vim.keymap.set('n', ']b', ':bnext<CR>', { desc = 'Go to next [B]uffer' })
 vim.keymap.set('n', '[b', ':bprev<CR>', { desc = 'Go to previous [B]uffer' })
