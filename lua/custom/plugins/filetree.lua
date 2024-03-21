46d59d9830811f100ed39adfb4a55a0714fa20f9
Juan Leandro Leites Barrios <laloleites@gmail.com>
2024-03-22 00:14:05 +0100

    Improved filetree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -7,10 +7,48 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
-    require('neo-tree').setup {}
-    require('which-key').register {
-      ['<leader>n'] = { name = '[N]eoTree', _ = 'which_key_ignore' },
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = false, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            '.git',
+          },
+        },
+      },
     }
-    vim.keymap.set('n', '<leader>nt', '<Cmd>Neotree toggle<CR>', { desc = '[N]eo[T]ree' })
+    vim.keymap.set('n', '<C-N>', '<Cmd>Neotree toggle<CR>', { desc = '[N]eo[T]ree' })
   end,
 }
+
+-- return {
+--   'nvim-tree/nvim-tree.lua',
+--   version = '*',
+--   config = function()
+--     require('nvim-tree').setup {
+--       sort = {
+--         sorter = 'case_sensitive',
+--       },
+--       view = {
+--         adaptive_size = false,
+--         side = 'left',
+--         width = { min = 30, max = 30 },
+--         preserve_window_proportions = true,
+--       },
+--       renderer = {
+--         group_empty = true,
+--       },
+--       filters = {
+--         dotfiles = false,
+--         custom = { '^.git$' },
+--       },
+--       filesystem_watchers = {
+--         enable = true,
+--       },
+--     }
+--     vim.keymap.set('n', '<C-N>', '<Cmd>NvimTreeToggle<CR>', { desc = 'Toogle Filetree' })
+--   end,
+-- }
