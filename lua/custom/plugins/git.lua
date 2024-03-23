89a5e865413841664d5f2fcc1d1308af74bb113d
Juan Leandro Leites Barrios <laloleites@gmail.com>
2024-03-23 22:56:09 +0100

    Added togglerone

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -5,8 +5,10 @@ return {
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
     }
 
+    local togglerone = require 'custom.togglerone.plugin.togglerone'
     vim.keymap.set('n', '<leader>gs', ':DiffviewOpen<cr>', { desc = '[G]it [S]tatus' })
     vim.keymap.set('n', '<leader>gc', ':DiffviewClose<cr>', { desc = '[G]it [C]lose' })
     vim.keymap.set('n', '<leader>gh', ':DiffviewFileHistory<cr>', { desc = '[G]it [H]istory' })
+    vim.keymap.set('n', '<leader>gg', togglerone.toggle_map('DiffviewOpen', 'DiffviewClose'), { desc = '[G]it Toggle' })
   end,
 }
